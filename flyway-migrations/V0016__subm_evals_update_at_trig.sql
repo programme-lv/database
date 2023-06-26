9f269a45a0637fbfb22b351c1cb34860acc380a0
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-26 12:43:26 +0300

    add trigger to update submission evals updated at

---
+++ flyway-migrations/V0016__subm_evals_update_at_trig.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION update_updated_at()
+  RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at := NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER task_subm_evaluations_updated_at_trigger
+AFTER UPDATE ON task_subm_evaluations
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
