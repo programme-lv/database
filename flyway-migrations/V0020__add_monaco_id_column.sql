2367539f6f05ffa425794c48ec4069e18c0f8599
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-27 22:37:23 +0300

    add monaco id column

---
+++ flyway-migrations/V0020__add_monaco_id_column.sql
@@ -0,0 +1,7 @@
+ALTER TABLE programming_languages ADD COLUMN monaco_id TEXT;
+
+UPDATE programming_languages SET monaco_id = 'cpp' WHERE id = 'cpp17';
+
+UPDATE programming_languages SET monaco_id = 'python' WHERE id = 'python3.10';
+
+UPDATE programming_languages SET monaco_id = 'java' WHERE id = 'java18';
