e7328d00b17b48205d605337002a7066b866f090
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-29 01:12:47 +0300

    add authors to mock data

--- mock-data/baobabi_task.sql
+++ mock-data/baobabi_task.sql
@@ -7,3 +7,6 @@ VALUES ('baobabi', 'Baobabi', 'ProblemCon');
 INSERT INTO task_versions(version_name, task_id, time_lim_ms, mem_lim_kb, eval_type)
 VALUES ('v1', 'baobabi', 1000, 262144, 'simple');
 
+INSERT INTO task_authors(task_id, author) VALUES
+('baobabi', 'KrisjanisP'),
+('baobabi', 'KPetrucena');
