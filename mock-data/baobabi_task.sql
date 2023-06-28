9b5781b84067dd49b9af06493969355f3c56348f
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-29 00:31:13 +0300

    default created_at value and mock baobabi task

---
+++ mock-data/baobabi_task.sql
@@ -0,0 +1,9 @@
+INSERT INTO task_sources(abbreviation, full_name, event_description) VALUES
+('ProblemCon','ProblemCon++','Programmēšanas sacensības un hakatons.');
+
+INSERT INTO tasks(id, full_name, origin)
+VALUES ('baobabi', 'Baobabi', 'ProblemCon');
+
+INSERT INTO task_versions(version_name, task_id, time_lim_ms, mem_lim_kb, eval_type)
+VALUES ('v1', 'baobabi', 1000, 262144, 'simple');
+
