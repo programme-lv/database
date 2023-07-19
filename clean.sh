e733bd2c27d508fb69469a9a61181140573cfaf9
KrisjanisP <krisjanispetrucena@gmail.com>
2023-07-19 20:41:02 +0300

    rename eval types update clean.sh script to work from any location

--- clean.sh
+++ clean.sh
@@ -1,3 +1,14 @@
 #! /usr/bin/bash
 
-docker run --rm -it --network=host -v $(pwd)/flyway-migrations:/flyway/flyway-migrations -v $(pwd)/flyway.conf:/flyway/conf/flyway.conf flyway/flyway:9.19 clean
+
+set -e # Exit on error
+
+DIR=$(dirname "$(readlink -f "$0")")
+pushd $DIR
+
+docker run --rm -it --network=host \
+    -v $(pwd)/flyway-migrations:/flyway/flyway-migrations \
+    -v $(pwd)/flyway.conf:/flyway/conf/flyway.conf \
+    flyway/flyway:9.20 clean
+
+popd
