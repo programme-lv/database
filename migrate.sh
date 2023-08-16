ff65a951db4764a1148c92608b50a592c0b10c86
KrisjanisP <krisjanispetrucena@gmail.com>
2023-08-16 10:09:28 +0300

    update flyway version

--- migrate.sh
+++ migrate.sh
@@ -6,8 +6,8 @@ DIR=$(dirname "$(readlink -f "$0")")
 pushd $DIR
 
 docker run --rm -it --network=host \
-    -v $(pwd)/flyway-migrations:/flyway/flyway-migrations \
-    -v $(pwd)/flyway.conf:/flyway/conf/flyway.conf \
-    flyway/flyway:9.20 migrate
+	-v $(pwd)/flyway-migrations:/flyway/flyway-migrations \
+	-v $(pwd)/flyway.conf:/flyway/conf/flyway.conf \
+	flyway/flyway:9.21 migrate
 
 popd
