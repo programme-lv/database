545417c778735a3f20b22ee60edbe204a3c23876
KrisjanisP <krisjanispetrucena@gmail.com>
2023-08-18 19:19:17 +0300

    integration testing progress

--- i-test-dockerfile/entrypoint.sh
+++ i-test-dockerfile/entrypoint.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-set -e # Exit immediately if a command exits with a non-zero status.
+trap "echo 'Interrupted! Exiting...'; exit 1" SIGINT SIGTERM
 
-until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t 1; do
+set -e # Exit immediately if a command exits with a non-zero status.
+REPO_URL="https://github.com/programme-lv/database"
+DEST_DIR="/tmp/proglv-database/repo"
+
+git clone $REPO_URL $DEST_DIR
+
+ls -l $DEST_DIR
+
+DB_USER=$POSTGRES_USER
+DB_NAME=$POSTGRES_DB
+
+until pg_isready -U $DB_USER -d $DB_NAME -t 1; do
 	echo >&2 "Postgres is unavailable - sleeping"
 	sleep 1
 done
 
 FW_FLAGS="-url=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME"
 FW_FLAGS="$FW_FLAGS -user=$DB_USER -password=$DB_PASSWORD"
-FW_FLAGS="$FW_FLAGS -locations=filesystem:/flyway-migrations"
+FW_FLAGS="$FW_FLAGS -locations=/flyway-migrations"
 
 flyway $FW_FLAGS migrate
