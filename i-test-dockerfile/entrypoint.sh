03b8582428fa29457b57f245d78fa23d37f81713
KrisjanisP <krisjanispetrucena@gmail.com>
2023-08-18 22:23:06 +0300

    give up on i test dockerfile

--- i-test-dockerfile/entrypoint.sh
+++ i-test-dockerfile/entrypoint.sh
@@ -12,14 +12,23 @@ ls -l $DEST_DIR
 
 DB_USER=$POSTGRES_USER
 DB_NAME=$POSTGRES_DB
+DB_PASSWORD=$POSTGRES_PASSWORD
 
-until pg_isready -U $DB_USER -d $DB_NAME -t 1; do
+# ip route | awk '/default/ { print $3 }'
+HOST_IP=$(ip route | awk '/default/ { print $3 }')
+echo "HOST_IP=$HOST_IP"
+
+until pg_isready -h $HOST_IP -U $DB_USER -d $DB_NAME -t 1; do
 	echo >&2 "Postgres is unavailable - sleeping"
 	sleep 1
 done
 
-FW_FLAGS="-url=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME"
+# ls -al /flyway-migrations
+
+FW_FLAGS="-url=jdbc:postgresql://$HOST_IP:5432/$DB_NAME"
 FW_FLAGS="$FW_FLAGS -user=$DB_USER -password=$DB_PASSWORD"
 FW_FLAGS="$FW_FLAGS -locations=/flyway-migrations"
+
+ip a
 
 flyway $FW_FLAGS migrate
