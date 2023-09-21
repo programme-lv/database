aced2a51f4ddd22fda83293639891e56361c6fc8
krisjanisp <krisjanispetrucena@gmail.com>
2023-09-21 13:53:00 +0300

    better evaluation statuses

--- flyway-migrations/R__evaluation_statuses.sql
+++ flyway-migrations/R__evaluation_statuses.sql
@@ -1,19 +1,28 @@
 DELETE FROM evaluation_statuses;
 INSERT INTO evaluation_statuses (id, description_en, description_lv)
-VALUES 
-('IQS', 'in queue state', 'gaida rindā'),
-('ICS', 'in compilation state', 'tiek kompilēts'),
-('ITS', 'in testing state', 'tiek testēts'),
+VALUES
+-- SUBMISSION SCOPE
+('IQ', 'in queue', 'gaida rindā'),
+('C', 'compiling', 'tiek kompilēts'),
+('T', 'testing', 'tiek testēts'),
+('F', 'finished', 'pabeigts'),
+('CE', 'compilation error', 'kompilācijas kļūda'),
+('SV', 'security violation', 'drošības pārkāpums'),
+('RJ', 'rejected', 'noraidīts'),
+
+-- TEST SCOPE
 ('AC', 'accepted', 'pieņemts'),
 ('PT', 'partially correct', 'daļēji pareizi'),
+
 ('WA', 'wrong answer', 'nepareiza atbilde'),
-('RE', 'runtime error', 'izpildes laika kļūda'),
 ('PE', 'presentation error', 'prezentācijas kļūda'),
+
 ('TLE', 'time limit exceeded', 'laika limits pārsniegts'),
 ('MLE', 'memory limit exceeded', 'atmiņas limits pārsniegts'),
 ('ILE', 'idleness limit exceeded', 'darbības trūkums'),
-('CE', 'compilation error', 'kompilācijas kļūda'),
-('ISE', 'internal server error', 'iekšēja servera kļūda'),
-('SV', 'security violation', 'drošības pārkāpums'),
+
 ('IG', 'ignored', 'noignorēts'),
-('RJ', 'rejected', 'noraidīts');
+
+-- COMMON STATES
+('RE', 'runtime error', 'izpildes laika kļūda'),
+('ISE', 'internal server error', 'iekšēja servera kļūda');
