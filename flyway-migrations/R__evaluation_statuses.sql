13aa31a13fbdd5f41b673194654d518c0a5fe1d6
krisjanisp <krisjanispetrucena@gmail.com>
2023-09-21 14:08:21 +0300

    specify whether statuses are for submissions or tests

--- flyway-migrations/R__evaluation_statuses.sql
+++ flyway-migrations/R__evaluation_statuses.sql
@@ -1,28 +1,29 @@
 DELETE FROM evaluation_statuses;
-INSERT INTO evaluation_statuses (id, description_en, description_lv)
+INSERT INTO evaluation_statuses
+(id, description_en, description_lv, dev_notes)
 VALUES
 -- SUBMISSION SCOPE
-('IQ', 'in queue', 'gaida rindā'),
-('C', 'compiling', 'tiek kompilēts'),
-('T', 'testing', 'tiek testēts'),
-('F', 'finished', 'pabeigts'),
-('CE', 'compilation error', 'kompilācijas kļūda'),
-('SV', 'security violation', 'drošības pārkāpums'),
-('RJ', 'rejected', 'noraidīts'),
+('IQ', 'in queue', 'gaida rindā', '(submission)'),
+('C', 'compiling', 'tiek kompilēts', '(submission)'),
+('T', 'testing', 'tiek testēts', '(submission)'),
+('F', 'finished', 'pabeigts', '(submission)'),
+('CE', 'compilation error', 'kompilācijas kļūda', '(submission)'),
+('RJ', 'rejected', 'noraidīts', '(submission)'),
 
 -- TEST SCOPE
-('AC', 'accepted', 'pieņemts'),
-('PT', 'partially correct', 'daļēji pareizi'),
+('AC', 'accepted', 'pieņemts', '(test)'),
+('PT', 'partially correct', 'daļēji pareizi', '(test)'),
 
-('WA', 'wrong answer', 'nepareiza atbilde'),
-('PE', 'presentation error', 'prezentācijas kļūda'),
+('WA', 'wrong answer', 'nepareiza atbilde', '(test)'),
+('PE', 'presentation error', 'prezentācijas kļūda', '(test)'),
 
-('TLE', 'time limit exceeded', 'laika limits pārsniegts'),
-('MLE', 'memory limit exceeded', 'atmiņas limits pārsniegts'),
-('ILE', 'idleness limit exceeded', 'darbības trūkums'),
+('TLE', 'time limit exceeded', 'laika limits pārsniegts', '(test)'),
+('MLE', 'memory limit exceeded', 'atmiņas limits pārsniegts', '(test)'),
+('ILE', 'idleness limit exceeded', 'darbības trūkums', '(test)'),
 
-('IG', 'ignored', 'noignorēts'),
+('IG', 'ignored', 'noignorēts', '(test)'),
+('RE', 'runtime error', 'izpildes laika kļūda', '(test)'),
 
 -- COMMON STATES
-('RE', 'runtime error', 'izpildes laika kļūda'),
-('ISE', 'internal server error', 'iekšēja servera kļūda');
+('SV', 'security violation', 'drošības pārkāpums', '(common)'),
+('ISE', 'internal server error', 'iekšēja servera kļūda', '(common)');
