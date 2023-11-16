f3972d7a211b1af10227b90371a736394d096011
krisjanisp <krisjanispetrucena@gmail.com>
2023-11-16 22:44:49 +0200

    deferrable fk in programming_languages

--- flyway-migrations/R__programming_languages.sql
+++ flyway-migrations/R__programming_languages.sql
@@ -1,27 +1,30 @@
-DELETE FROM programming_languages;
+BEGIN;
 
-INSERT INTO programming_languages
-    VALUES ('cpp17', 'C++17 (GNU G++)', 'main.cpp', 'g++ -std=c++17 -o main main.cpp', './main', 'g++ --version',
-    e'#include <iostream>
-int main() { std::cout << "Hello, World!"; }', 'cpp', 'main');
+SET CONSTRAINTS ALL DEFERRED;
 
-INSERT INTO programming_languages
-    VALUES ('python3.10', 'Python 3.10', 'main.py', null, 'python3.10 main.py', 'python3.10 --version',
-    'print("Hello, World!")', 'python', null);
-
-INSERT INTO programming_languages
-    VALUES ('java18', 'Java 18', 'Main.java', 'javac Main.java', 'java Main', 'java --version',
-    e'public class Main {
+DELETE FROM
+    programming_languages;
+INSERT INTO public.programming_languages (id, full_name, code_filename, compile_cmd, execute_cmd, env_version_cmd, hello_world_code, monaco_id, compiled_filename, enabled)
+VALUES ('python3.10', 'Python 3.10', 'main.py', null, 'python3.10 main.py', 'python3.10 --version', 'print("Hello, World!")', 'python', null, false);
+INSERT INTO public.programming_languages (id, full_name, code_filename, compile_cmd, execute_cmd, env_version_cmd, hello_world_code, monaco_id, compiled_filename, enabled)
+VALUES ('java18', 'Java 18', 'Main.java', 'javac Main.java', 'java Main', 'java --version', e'public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-}', 'java', 'Main');
-
-INSERT INTO programming_languages
-    VALUES ('go1.19', 'Go 1.19', 'main.go', 'go build main.go', './main', 'go version',
-    e'package main
+}', 'java', 'Main', false);
+INSERT INTO public.programming_languages (id, full_name, code_filename, compile_cmd, execute_cmd, env_version_cmd, hello_world_code, monaco_id, compiled_filename, enabled)
+VALUES ('python3.11', 'Python 3.11', 'main.py', null, 'python3.11 main.py', 'python3.10 --version', 'print("Hello, World!")', 'python', null, true);
+INSERT INTO public.programming_languages (id, full_name, code_filename, compile_cmd, execute_cmd, env_version_cmd, hello_world_code, monaco_id, compiled_filename, enabled)
+VALUES ('go1.19', 'Go 1.19', 'main.go', 'go build main.go', './main', 'go version', e'package main
 import "fmt"
 func main() {
     fmt.Println("Hello, World!")
-}', 'go', 'main');
+}', 'go', 'main', true);
 
+INSERT INTO public.programming_languages (id, full_name, code_filename, compile_cmd, execute_cmd, env_version_cmd, hello_world_code, monaco_id, compiled_filename, enabled)
+VALUES ('cpp17', 'C++17 (GNU G++) asdf', 'main.cpp', 'g++ -std=c++17 -o main main.cpp', './main', 'g++ --version', e'#include <iostream>
+int main() { std::cout << "Hello, World!" << endl; }', 'cpp', 'main', true);
+
+
+
+COMMIT;
---
+++ flyway-migrations/V0071__deferable_fk_langs.sql
@@ -0,0 +1,6 @@
+alter table task_submissions
+    drop constraint task_submissions_programming_lang_id_fkey;
+
+alter table task_submissions
+    add foreign key (programming_lang_id) references programming_languages
+        deferrable;
