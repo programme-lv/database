e74dfd59bdcc29a7c3d2268c12c8f283c418f723
krisjanisp <krisjanispetrucena@gmail.com>
2024-03-05 23:43:47 +0200

    newer flyway version

--- flyway-migrate.sh
+++ flyway-migrate.sh
@@ -14,7 +14,7 @@ else
 	docker run --rm -it --network=host \
 		-v $(pwd)/flyway-migrations:/flyway/flyway-migrations \
 		-v $(pwd)/flyway.conf:/flyway/conf/flyway.conf \
-		flyway/flyway:9.21 migrate
+		flyway/flyway:10.9 migrate
 fi
 
 
